694097a91af64f77cba8386b9245df02bef3a09a
Rick Mac Gillis <noreply@rickmacgillis.com>
2025-07-10 14:19:21 -0500

    Patch
    Signed-off-by: Rick Mac Gillis <noreply@rickmacgillis.com>

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Shortcode\ExampleController;
 use App\Http\Controllers\Admin\AdminLogController;
 use App\Http\Controllers\Admin\Table\DragonDataTable;
 use App\Http\Controllers\Admin\Table\DragonDataDetails;
+use Dragon\Http\Middleware\AdminOnly;
 
 /*
  |--------------------------------------------------------------------------
@@ -21,27 +22,30 @@ use App\Http\Controllers\Admin\Table\DragonDataDetails;
 Route::view('/plugin-author/', 'plugin-author')->name('plugin-author');
 
 Route::middleware(['web'])->prefix(Config::prefix())->group(function () {
-	Route::controller(SettingsController::class)->name('admin-settings')->group(function () {
-		Route::get('admin-settings', 'show');
-		Route::post('admin-settings', 'store')->middleware(ValidatesNonce::class);
-	});
 	
-	Route::controller(AdminLogController::class)->name('admin-log')->group(function () {
-		Route::get('admin-log', 'show');
-		Route::post('admin-log', 'clear')->middleware(ValidatesNonce::class);
+	Route::middleware(AdminOnly::class)->group(function () {
+		Route::controller(SettingsController::class)->name('admin-settings')->group(function () {
+			Route::get('admin-settings', 'show');
+			Route::post('admin-settings', 'store')->middleware(ValidatesNonce::class);
+		});
+		
+		Route::controller(AdminLogController::class)->name('admin-log')->group(function () {
+			Route::get('admin-log', 'show');
+			Route::post('admin-log', 'clear')->middleware(ValidatesNonce::class);
+		});
+		
+		Route::controller(DragonDataTable::class)->name('admin-dragons')->group(function () {
+			Route::get('admin-dragons', 'show');
+			Route::delete('admin-dragons', 'delete');
+		});
+		
+		Route::controller(DragonDataDetails::class)->name('admin-dragon-details')->group(function () {
+			Route::get('admin-dragon-details', 'show');
+			Route::post('admin-dragon-details', 'store')->middleware(ValidatesNonce::class);
+		});
 	});
-	
+		
 	Route::controller(ExampleController::class)->name('example-shortcode')->group(function () {
 		Route::get('example-shortcode', 'show');
-	});
-	
-	Route::controller(DragonDataTable::class)->name('admin-dragons')->group(function () {
-		Route::get('admin-dragons', 'show');
-		Route::delete('admin-dragons', 'delete');
-	});
-	
-	Route::controller(DragonDataDetails::class)->name('admin-dragon-details')->group(function () {
-		Route::get('admin-dragon-details', 'show');
-		Route::post('admin-dragon-details', 'store')->middleware(ValidatesNonce::class);
 	});
 });
